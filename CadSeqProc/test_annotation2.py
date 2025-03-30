87acdb7e8a294b29067acc32cd5f69e539a55b1b
henrychh06 <166947098+henrychh06@users.noreply.github.com>
2025-03-30 07:02:37 -0500

    Update test_annotation2.py

--- CadSeqProc/test_annotation2.py
+++ CadSeqProc/test_annotation2.py
@@ -45,7 +45,7 @@ def load_minimal_json(json_path):
 
 def extract_keywords_from_annotation(annotation_dir, sample_id):
     try:
-        annotation_files = glob(os.path.join(annotation_dir, sample_id, "minimal_json",  f"{sample_id}.json"))
+        annotation_files = glob(os.path.join(annotation_dir, sample_id, "qwen2_vlm_annotation",  f"final_{sample_id}.json"))
         if not annotation_files:
             return ""
         with open(annotation_files[0], 'r') as f:
